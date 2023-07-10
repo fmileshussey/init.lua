89cbb14c4882f3a8b796900fd24db085b4e0ab7e
Finnegan <fmileshussey@outlook.com>
2023-07-11 09:07:48 +1200

    Updates lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,52 +3,37 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
+  'tsserver',
   'rust_analyzer',
-  'jdtls',
 })
 
--- fix undefined global 'vim'
+-- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
 
 local cmp = require('cmp')
--- local cmp_select = {behavior = cmp.selectbehavior.select}
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  --['<c-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  -- ['<c-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<c-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<c-space>"] = cmp.mapping.complete(),
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
 })
 
-cmp_mappings['<tab>'] = nil
-cmp_mappings['<s-tab>'] = nil
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
 })
 
-lsp.configure('jdtls', {
-  settings = {
-    java = {
-      saveActions = {
-        organizeImports = true,
-      }
-    }
-  },
-  
-  on_attach = function(client, bufnr)
-    print('JDT Language Server attached')
-  end
-})
-
-
 lsp.set_preferences({
     suggest_lsp_servers = false,
     sign_icons = {
-        error = 'e',
-        warn = 'w',
-        hint = 'h',
-        info = 'i'
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
     }
 })
 
@@ -56,7 +41,7 @@ lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "k", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
@@ -64,7 +49,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<c-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
