c50e48e9419b7306475f480615db6adf38b33265
Finnegan <fmileshussey@outlook.com>
2025-01-26 20:15:57 +1300

    Adds mason, lsps, remaps

--- lua/finn/remap.lua
+++ lua/finn/remap.lua
@@ -1,2 +1,16 @@
 vim.g.mapleader =" "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- TODO this isn't auto indenting for some reason
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+
