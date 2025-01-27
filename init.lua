3b2e80847a5c0eb675b6210fe78b678018e11d1d
fmileshussey <fmileshussey@outlook.com>
2025-01-27 13:44:02 +1300

    Removes print, adds comments

--- init.lua
+++ init.lua
@@ -1,2 +1,1 @@
 require("finn")
-print("hello")
--- lua/finn/init.lua
+++ lua/finn/init.lua
@@ -1,3 +1,6 @@
+-- Remaps native vim keys
 require("finn.remap")
+-- Adds handling for lazy nvim
 require("finn.lazy")
+-- Bunch of nvim configs
 require("finn.misc")
